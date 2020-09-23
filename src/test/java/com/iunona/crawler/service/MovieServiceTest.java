871f7bb25872caee5c7baedf5b5e3843be97a505
iupanasenco <panasenco.iunona@gmail.com>
2020-09-23 15:41:10 +0300

    service tests

--- src/test/java/com/iunona/crawler/service/MovieServiceTest.java
+++ src/test/java/com/iunona/crawler/service/MovieServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,9 +32,16 @@ class MovieServiceTest {
         }
 
         @Bean
-        public MovieService movieService(){
-            return new MovieService(moviesRepository(), movieCrawler(), null);
+        public MailService mailService(){
+            return new MailService(new JavaMailSenderImpl());
         }
+
+        @Bean
+        public MovieService movieService(){
+            return new MovieService(moviesRepository(), movieCrawler(), mailService());
+        }
+
+
     }
 
     @Autowired
