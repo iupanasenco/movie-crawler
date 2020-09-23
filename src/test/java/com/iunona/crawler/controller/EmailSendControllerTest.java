c8fe440620cfe880ccd7fca93a6568466725a878
iupanasenco <panasenco.iunona@gmail.com>
2020-09-23 16:36:51 +0300

    abstract test config

--- src/test/java/com/iunona/crawler/controller/EmailSendControllerTest.java
+++ src/test/java/com/iunona/crawler/controller/EmailSendControllerTest.java
@@ -1,22 +1,19 @@
 package com.iunona.crawler.controller;
 
-import com.iunona.crawler.crawler.MovieCrawler;
-import com.iunona.crawler.repo.MoviesRepository;
+import com.iunona.crawler.AbstractTestConfig;
 import com.iunona.crawler.service.MailService;
 import com.iunona.crawler.service.MovieService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(AbstractTestConfig.class)
 @AutoConfigureMockMvc
 @WebMvcTest
 class EmailSendControllerTest {
@@ -24,35 +21,12 @@ class EmailSendControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    protected MovieService movieService;
 
-    @TestConfiguration
-    static class MovieServiceTestContextConfiguration {
+    @Autowired
+    protected MailService mailService;
 
-        @Bean
-        public JavaMailSender javaMailSender() {
-            return new JavaMailSenderImpl();
-        }
-
-        @Bean
-        public MoviesRepository moviesRepository() {
-            return new MoviesRepository();
-        }
-
-        @Bean
-        public MovieCrawler movieCrawler() {
-            return new MovieCrawler();
-        }
-
-        @Bean
-        public MailService mailService() {
-            return new MailService(javaMailSender());
-        }
-
-        @Bean
-        public MovieService movieService() {
-            return new MovieService(moviesRepository(), movieCrawler(), mailService());
-        }
-    }
 
     //TODO:Implement Dumbster
 //    @Test
